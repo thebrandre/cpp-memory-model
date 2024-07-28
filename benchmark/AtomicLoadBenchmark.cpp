9371d69214f8e8133c1755b02a97d908b4f1f22a
André Brand <andre.j.brand@gmx.de>
2024-07-28 21:16:41 +0200

    ✨ Add store performance counter to the atomic load benchmark

--- benchmark/AtomicLoadBenchmark.cpp
+++ benchmark/AtomicLoadBenchmark.cpp
@@ -10,50 +10,56 @@ static constexpr int ItemRangeBegin = 1 << 8;
 static constexpr int ItemRangEnd = 1 << 12;
 
 // ReSharper disable once CppPassValueParameterByConstReference
-static void StoreSpammer(std::stop_token StopToken, std::atomic<int> *Variable, const std::memory_order Order) {
-  int i = 0;
+static void StoreSpammer(std::stop_token StopToken, std::atomic<std::int64_t> *Variable, std::int64_t *Counter,
+                         const std::memory_order Order) {
+  auto &i = *Counter;
+  i = 0;
   while (!StopToken.stop_requested())
     Variable->store(++i, Order);
 }
 
 // ReSharper disable once CppPassValueParameterByConstReference
-static void StoreSpammerFence(std::stop_token StopToken, [[maybe_unused]] std::atomic<int> *Variable,
-                              [[maybe_unused]] const std::memory_order Order) {
-  int i = 0;
+static void StoreSpammerFence(std::stop_token StopToken, [[maybe_unused]] std::atomic<std::int64_t> *Variable,
+                              std::int64_t *Counter, [[maybe_unused]] const std::memory_order Order) {
+  auto &i = *Counter;
+  i = 0;
   while (!StopToken.stop_requested()) {
     Variable->store(++i, std::memory_order_relaxed);
     MemoryFence();
   }
 }
-static void StoreSpammerSomewhereElse(std::stop_token StopToken, [[maybe_unused]] std::atomic<int> *Variable,
-                                      const std::memory_order Order) {
-  auto MyOwnAtomicVariable = std::make_unique<std::atomic<int>>();
-  StoreSpammer(std::move(StopToken), MyOwnAtomicVariable.get(), Order);
+static void StoreSpammerSomewhereElse(std::stop_token StopToken, [[maybe_unused]] std::atomic<std::int64_t> *Variable,
+                                      std::int64_t *Counter, const std::memory_order Order) {
+  auto MyOwnAtomicVariable = std::make_unique<std::atomic<std::int64_t>>();
+  StoreSpammer(std::move(StopToken), MyOwnAtomicVariable.get(), Counter, Order);
 }
 
-static void StoreSpammerFenceSomewhereElse(std::stop_token StopToken, [[maybe_unused]] std::atomic<int> *Variable,
+static void StoreSpammerFenceSomewhereElse(std::stop_token StopToken,
+                                           [[maybe_unused]] std::atomic<std::int64_t> *Variable, std::int64_t *Counter,
                                            const std::memory_order Order) {
-  auto MyOwnAtomicVariable = std::make_unique<std::atomic<int>>();
-  StoreSpammerFence(std::move(StopToken), MyOwnAtomicVariable.get(), Order);
+  auto MyOwnAtomicVariable = std::make_unique<std::atomic<std::int64_t>>();
+  StoreSpammerFence(std::move(StopToken), MyOwnAtomicVariable.get(), Counter, Order);
 }
 
 template <typename SpammerType>
 static void BM_AtomicLoad(benchmark::State &State, const std::memory_order Order, SpammerType SpammerCallback) {
   const int NumItems = static_cast<int>(State.range(0));
-  auto AtomicVariable = std::make_unique<std::atomic<int>>();
-  auto WriteMemoryOrder = Order == std::memory_order_acquire ? std::memory_order_release : Order;
-  std::jthread Spammer(SpammerCallback, AtomicVariable.get(), WriteMemoryOrder);
+  auto AtomicVariable = std::make_unique<std::atomic<std::int64_t>>();
+  const auto WriteMemoryOrder = Order == std::memory_order_acquire ? std::memory_order_release : Order;
+  std::int64_t WriteCounter{};
+  std::jthread Spammer(SpammerCallback, AtomicVariable.get(), &WriteCounter, WriteMemoryOrder);
 
   for (auto _ : State) {
-    int Sum = 0;
+    std::int64_t Sum = 0;
     for ([[maybe_unused]] const auto i : std::views::iota(0, NumItems))
       Sum += AtomicVariable->load(Order);
     benchmark::DoNotOptimize(Sum);
   }
   Spammer.request_stop();
-  State.counters["duration_per_operation"] =
-      benchmark::Counter(static_cast<double>(State.iterations()) * State.range(0),
-                         benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+  State.counters["duration_per_operation"] = benchmark::Counter(
+      static_cast<double>(State.range(0)), benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+  State.counters["duration_per_write"] =
+      benchmark::Counter(static_cast<double>(WriteCounter), benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
   State.SetComplexityN(State.range(0));
 }
 
