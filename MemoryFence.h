9348cb06f82fdda6ad6509cae2c499464415a71e
André Brand <andre.brand@haag-streit.com>
2024-07-29 06:45:37 +0200

    🐛 Fix code generation when targeting ARM with msvc

--- MemoryFence.h
+++ MemoryFence.h
@@ -5,14 +5,31 @@
 /// Compilers nowadays do not emit an mfence instruction on atomic stores on x86.
 /// This is a convenience macro to force this instruction on x86 across different
 /// compilers gcc, clang, and msvc.
-/// @see https://godbolt.org/z/qq1PevdzG
+/// @see https://godbolt.org/z/v1vKejcvh
 
+#ifdef __has_builtin
+#if __has_builtin(__builtin_ia32_mfence)
+#define MemoryFence __builtin_ia32_mfence
+#endif
+#endif
+
+#ifndef MemoryFence
 #if defined(_MSC_VER) && (defined(_M_AMD64) || defined(_M_IX86))
 #include <intrin.h>
 #define MemoryFence _mm_mfence
-#elif __has_builtin(__builtin_ia32_mfence)
+#endif
+#endif
+
+// Fallback for old versions of gcc
+#ifndef MemoryFence
+#if defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
 #define MemoryFence __builtin_ia32_mfence
-#else
+#endif
+#endif
+
+// Generic fallback for targeting other architectures such as ARM. Also works with old gcc versions on x86.
+// This fence emits a *data memory barrier* in the inner-sharable domain on ARM (dmb ish).
+#ifndef MemoryFence
 #include <atomic>
 #define MemoryFence []() { std::atomic_thread_fence(std::memory_order_seq_cst); }
 #endif
