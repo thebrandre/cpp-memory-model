00a61a47908aefb19da8ac523557d9f53d347462
Andre Brand <andre.j.brand@gmx.de>
2024-08-04 10:28:35 +0200

    ✨ Add try_lock() to all implementations of StupidSelfishSpinMutex

--- StupidSelfishMutex.h
+++ StupidSelfishMutex.h
@@ -4,46 +4,43 @@
 // The assembly generated for the two versions is basically the same:
 // https://godbolt.org/z/zPTe3cW8G
 
+class StupidSelfishSpinMutexBase {
+public:
+  template <typename Derived> void lock(this Derived &&Self) noexcept {
+    while (!Self.try_lock())
+      ;
+  }
+};
+
 namespace v1 {
-class StupidSelfishSpinMutex {
+class StupidSelfishSpinMutex : public StupidSelfishSpinMutexBase {
   std::atomic_flag Locked{};
 
 public:
-  void lock() noexcept {
-    while (Locked.test_and_set(std::memory_order_acquire))
-      ;
-  }
+  bool try_lock() noexcept { return !Locked.test_and_set(std::memory_order_acquire); }
   void unlock() noexcept { Locked.clear(std::memory_order_release); }
 };
 
 } // namespace v1
 
 namespace v2 {
-class StupidSelfishSpinMutex {
+class StupidSelfishSpinMutex : public StupidSelfishSpinMutexBase {
   std::atomic<bool> Locked{};
 
 public:
-  void lock() noexcept {
-    while (Locked.exchange(true, std::memory_order_acquire))
-      ;
-  }
+  bool try_lock() noexcept { return !Locked.exchange(true, std::memory_order_acquire); }
   void unlock() noexcept { Locked.store(false, std::memory_order_release); }
 };
 } // namespace v2
 
 namespace v3 {
-class StupidSelfishSpinMutex {
+class StupidSelfishSpinMutex : public StupidSelfishSpinMutexBase {
   std::atomic<bool> Locked{};
 
 public:
   bool try_lock() noexcept {
     bool Expected = false;
     return Locked.compare_exchange_weak(Expected, true, std::memory_order_acquire, std::memory_order_relaxed);
-  }
-
-  void lock() noexcept {
-    while (!try_lock())
-      ;
   }
   void unlock() noexcept { Locked.store(false, std::memory_order_release); }
 };
